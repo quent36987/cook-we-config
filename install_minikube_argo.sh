26203ae08c70d64edabfca7b7e6d0f0fdfef78d8
qgoujon <qgoujon@sqli.com>
2024-08-02 11:49:33 +0200

    fix volume

--- install_minikube_argo.sh
+++ install_minikube_argo.sh
@@ -69,8 +69,17 @@ kubectl create secret docker-registry ghcr-secret \
 -n front
 echo -e "${GREEN}Secret docker-registry pour l'application front créé.${NC}"
 
+# Installation de Helm et du plugin helm-secrets
+echo -e "${GREEN}Installation de Helm et du plugin helm-secrets...${NC}"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm plugin install https://github.com/jkroepke/helm-secrets
+echo -e "${GREEN}Helm et le plugin helm-secrets installés.${NC}"
+
 # Apply the application manifests
-kubectl apply -n argocd -f https://github.com/quent36987/cook-we-config/blob/main/argocd-applications.yaml
+echo -e "${GREEN}Application des manifests des applications...${NC}"
+#kubectl apply -n argocd -f https://github.com/quent36987/cook-we-config/blob/main/argocd-applications.yaml
 
 # Fin du script
 echo -e "${GREEN}Script terminé.${NC}"
